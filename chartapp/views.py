001ccc4adbecabea565a4da1f5f7448c281f24a6
phitron-demo <sojibhasan5800@gmail.com>
2025-09-06 16:01:54 +0600

    Implement user creation and authenticated user listing APIs in ChatApp views

--- chartapp/views.py
+++ chartapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import *
+from .serializers import *
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = [IsAuthenticated]
