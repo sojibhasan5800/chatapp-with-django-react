9aad645a628fc9ad9b8ba46c139dc5999d8ca9ea
phitron-demo <sojibhasan5800@gmail.com>
2025-09-06 16:20:51 +0600

    Add conversation list and creation API with participant validation and duplicate check in ChatApp

--- chartapp/views.py
+++ chartapp/views.py
@@ -16,3 +16,58 @@ class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
     permission_classes = [IsAuthenticated]
+
+class ConversationListCreateView(generics.ListCreateAPIView):
+
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return (Conversation.objects
+                .filter(participants=self.request.user)
+                .prefetch_related('participants'))
+
+    # create method
+    def create(self, request, *args, **kwargs):
+        participants_data = request.data.get('participants', [])
+     
+    # Check exactly two participants
+        if len(participants_data) != 2:
+            return Response(
+                {'error': 'A conversation needs exactly two participants'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    # Check request user is participant
+        if str(request.user.id) not in map(str, participants_data):
+            return Response(
+                {'error': 'You are not a participant of this conversation'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+    # Fetch users from DB
+        users = User.objects.filter(id__in=participants_data)
+        if users.count() != 2:
+            return Response(
+                {'error': 'A conversation needs exactly two participants'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    # Check if conversation already exists
+    
+        existing_conversation = Conversation.objects.filter(
+            participants__id=participants_data[0]
+        ).filter(
+            participants__id=participants_data[1]
+        ).distinct()
+
+        if existing_conversation.exists():
+            return Response(
+                {'error': 'A conversation already exists between these participants'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        conversation = Conversation.objects.create()
+        conversation.participants.set(users)
+
+        #serialize the conversation
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
