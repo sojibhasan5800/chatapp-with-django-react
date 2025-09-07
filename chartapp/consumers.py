e0a78a9d3be26d8c69d51191b9819fab074a4e4b
phitron-demo <sojibhasan5800@gmail.com>
2025-09-07 08:58:32 +0600

    Add JWT authentication and fetch user from database

--- chartapp/consumers.py
+++ chartapp/consumers.py
@@ -7,7 +7,16 @@ from urllib.parse import parse_qs
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-       
+        query_string = self.scope['query_string'].decode('utf-8')
+        params = parse_qs(query_string)
+        token =  params.get('token', [None])[0] # token retrieved
+
+        if token:
+            try:
+                decoded_data = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+                self.user = await self.get_user(decoded_data['user_id']) #get the user from the token
+                self.scope['user'] = self.user
+
         await self.accept()
 
     async def disconnect(self, close_code):
--- chat_system/asgi.py
+++ chat_system/asgi.py
@@ -14,5 +14,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_system.settings')
+from chartapp.routing import websocket_urlpatterns
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
